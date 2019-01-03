25c390a08798230b4dc93dd498afd5f444190d4e
Sam Gillingham <gillingham.sam@gmail.com>
2019-01-04 08:51:09 +1000

    go back to external libs but pass --with-jpeg12=yes to force 12 bit JPEG

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,15 +31,14 @@ bash configure --prefix=$PREFIX \
                --with-geotiff=$PREFIX \
                --with-hdf4=$PREFIX \
                --with-hdf5=$PREFIX \
-               --with-jpeg=internal \
+               --with-jpeg=$PREFIX \
                --with-jpeg12 \
-               --with-hide-internal-symbols=yes \
                --with-kea=$PREFIX/bin/kea-config \
                --with-libiconv-prefix=$PREFIX \
                --with-libjson-c=$PREFIX \
                --with-libkml=$PREFIX \
                --with-liblzma=yes \
-               --with-libtiff=internal \
+               --with-libtiff=$PREFIX \
                --with-libz=$PREFIX \
                --with-netcdf=$PREFIX \
                --with-openjpeg=$PREFIX \
