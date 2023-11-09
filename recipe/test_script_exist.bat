c2963d34624868082947d0fed7c31bf7a05a096a
Peter Limkilde Svendsen <pelis@sdfi.dk>
2023-11-09 12:13:26 +0100

    Check in %PREFIX%\Scripts, clean up comments

--- recipe/test_script_exist.bat
+++ recipe/test_script_exist.bat
@@ -1,28 +1,21 @@
 @echo off
 REM As a workaround for GDAL apps unexpectedly returning error codes, this
 REM script checks only for the existence of the .py files below.
+REM This file should be replaced by calls to the scripts in meta.yaml once the
+REM behavior is fixed in GDAL itself.
 
 @echo on
 
-REM DEBUG: try to find location of these files
-where gdalattachpct.py
-where gdal_retile.py
-where gdal_proximity.py
-where gdal_edit.py
-where gdal_pansharpen.py
-where ogrmerge.py
-where ogr_layer_algebra.py
+if not exist %PREFIX%\Scripts\gdalattachpct.py exit 1
 
-if not exist %LIBRARY_BIN%\gdalattachpct.py exit 1
+if not exist %PREFIX%\Scripts\gdal_retile.py exit 1
 
-if not exist %LIBRARY_BIN%\gdal_retile.py exit 1
+if not exist %PREFIX%\Scripts\gdal_proximity.py exit 1
 
-if not exist %LIBRARY_BIN%\gdal_proximity.py exit 1
+if not exist %PREFIX%\Scripts\gdal_edit.py exit 1
 
-if not exist %LIBRARY_BIN%\gdal_edit.py exit 1
+if not exist %PREFIX%\Scripts\gdal_pansharpen.py exit 1
 
-if not exist %LIBRARY_BIN%\gdal_pansharpen.py exit 1
+if not exist %PREFIX%\Scripts\ogrmerge.py exit 1
 
-if not exist %LIBRARY_BIN%\ogrmerge.py exit 1
-
-if not exist %LIBRARY_BIN%\ogr_layer_algebra.py exit 1
+if not exist %PREFIX%\Scripts\ogr_layer_algebra.py exit 1
