ff2cadde3485cb5dcac51ced93d710582e8a6b66
Peter Limkilde Svendsen <pelis@sdfi.dk>
2023-11-09 10:22:56 +0100

    Try to print path to .py files in output

--- recipe/test_script_exist.bat
+++ recipe/test_script_exist.bat
@@ -1,25 +1,28 @@
+@echo off
 REM As a workaround for GDAL apps unexpectedly returning error codes, this
 REM script checks only for the existence of the .py files below.
 
 @echo on
 
-echo Test existence of gdalattachpct.py...
+REM DEBUG: try to find location of these files
+where gdalattachpct.py
+where gdal_retile.py
+where gdal_proximity.py
+where gdal_edit.py
+where gdal_pansharpen.py
+where ogrmerge.py
+where ogr_layer_algebra.py
+
 if not exist %LIBRARY_BIN%\gdalattachpct.py exit 1
 
-echo Test existence of gdal_retile.py...
 if not exist %LIBRARY_BIN%\gdal_retile.py exit 1
 
-echo Test existence of gdal_proximity.py...
 if not exist %LIBRARY_BIN%\gdal_proximity.py exit 1
 
-echo Test existence of gdal_edit.py...
 if not exist %LIBRARY_BIN%\gdal_edit.py exit 1
 
-echo Test existence of gdal_pansharpen.py...
 if not exist %LIBRARY_BIN%\gdal_pansharpen.py exit 1
 
-echo Test existence of ogrmerge.py...
 if not exist %LIBRARY_BIN%\ogrmerge.py exit 1
 
-echo Test existence of ogr_layer_algebra.py...
 if not exist %LIBRARY_BIN%\ogr_layer_algebra.py exit 1
